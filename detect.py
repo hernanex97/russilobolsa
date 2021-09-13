2e2a88e2cc966756e1e2e9c5b488cf6467f9dfdf
hernanex97 <44402597+hernanex97@users.noreply.github.com>
2021-09-13 15:49:08 -0300

    Cambio de print en el .txt de label
    se comenta el write anterior y solo se imprime en el archivo la confianza

--- detect.py
+++ detect.py
@@ -198,7 +198,8 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
                         line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
                         with open(txt_path + '.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                            #f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                            f.write(conf + '\n')
 
                     if save_img or save_crop or view_img:  # Add bbox to image
                         c = int(cls)  # integer class
