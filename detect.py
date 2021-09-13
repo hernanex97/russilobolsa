36b8831e53893de0a506896949bcf01ecf285cf4
hernanex97 <44402597+hernanex97@users.noreply.github.com>
2021-09-13 15:56:43 -0300

    acceder valor tupla

--- detect.py
+++ detect.py
@@ -199,7 +199,7 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                         line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
                         with open(txt_path + '.txt', 'a') as f:
                             #f.write(('%g ' * len(line)).rstrip() % line + '\n')
-                            f.write(str(conf) + '\n')
+                            f.write(str(conf.index(0)) + '\n')
 
                     if save_img or save_crop or view_img:  # Add bbox to image
                         c = int(cls)  # integer class
