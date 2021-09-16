4ea717dd89c21b3ae97ddfd5682981e7959e74bd
hernanex97 <44402597+hernanex97@users.noreply.github.com>
2021-09-15 21:48:11 -0300

    Revert "se saca al final el id"
    This reverts commit ee95fd51ab0db87271f11c075323f1e1708106c6.

--- detect.py
+++ detect.py
@@ -67,6 +67,9 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
     device = select_device(device)
     half &= device.type != 'cpu'  # half precision only supported on CUDA
 
+    #Initialize newID
+    newID = 0
+
     # Load model
     w = weights[0] if isinstance(weights, list) else weights
     classify, suffix = False, Path(w).suffix.lower()
@@ -183,8 +186,9 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
         sumValues = 0.0 # suma de total confianza
         minValue = 2.0 # mínimo valor de todos los valores de confianza obtenidos de una imágen
         maxValue = 0.0 # máximo valor de todos los valores de confianza obtenidos de una imágen
-
+        
         for i, det in enumerate(pred):  # detections per image
+            newID = newID + 1
             if webcam:  # batch_size >= 1
                 p, s, im0, frame = path[i], f'{i}: ', im0s[i].copy(), dataset.count
             else:
@@ -246,10 +250,10 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                     
                     existsSiloBolsa = True
                     averageVal = sumValues/cantValues  
-                    f.write(str(existsSiloBolsa) + ',' + str(cantValues) + ',' + str('%.2f' % minValue) + ',' + str('%.2f' % maxValue) + ',' + str('%.2f' % averageVal) + ',' + p.name + '\n') #se esc
+                    f.write(str(newID) + ',' + str(existsSiloBolsa) + ',' + str(cantValues) + ',' + str('%.2f' % minValue) + ',' + str('%.2f' % maxValue) + ',' + str('%.2f' % averageVal) + ',' + p.name + '\n') #se esc
                 else:
                     existsSiloBolsa = False
-                    f.write(str(existsSiloBolsa) + ',' + str(0) + ',' + str(0) + ',' + str(0) + ',' + str(0) + ',' + p.name + '\n') #se 
+                    f.write(str(newID) + ',' + str(existsSiloBolsa) + ',' + str(0) + ',' + str(0) + ',' + str(0) + ',' + str(0) + ',' + p.name + '\n') #se 
 
 
 
